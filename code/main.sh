e024807bea8678441f9da56837c1c6048f35c0f3
Jake Valsamis <jake@codeocean.com>
2023-06-28 00:55:16 +0000

    change ipynb to cfn-demo db

--- code/main.sh
+++ code/main.sh
@@ -10,4 +10,4 @@ jupyter nbconvert \
 	--execute query_redshift.ipynb
 
 #run utility capsule 
-curl -H "Content-Type: application/json" -u ${CUSTOM_KEY}: -X POST https://acmecorp-cfn-demo.codeocean.com/api/v1/computations --data-raw "$data_raw"
+#curl -H "Content-Type: application/json" -u ${CUSTOM_KEY}: -X POST https://acmecorp-cfn-demo.codeocean.com/api/v1/computations --data-raw "$data_raw"
